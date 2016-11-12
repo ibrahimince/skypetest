fb9b0639f76e735d83694b34bd9e8e417fab53e4
ibrahimince <ibrahimince@hotmail.dk>
2016-11-12 21:52:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+/*   
+    $("li").hide();
+    timer = setTimeout(function () {
+        $('li').show(2100);
+    }, 3000);*/
+
+        $(".loader").show();
+    timer = setTimeout(function () {
+        $('.loader').hide(2100);
+    }, 3000);
+
+ /*          $(".Testingconnectivity").show();
+    timer = setTimeout(function () {
+        $('.Testingconnectivity').hide(2100);
+    }, 3000);*/
+
+/*        $(".StartSkypeforBusiness").hide();
+    timer = setTimeout(function () {
+        $('.StartSkypeforBusiness').show(2100);
+    }, 3000);*/
+
+    /*$(".center").show().delay(7000).remove();*/
+
+   /*     $(".spinner").show().delay(7000).hide();*/
+
+
+/*$(".content").find("li").hide();
+ timer = setTimeout(function () {
+$(".content").find("li").show(2100);
+    }, 3000);*/
